c819ccac4c2cf9361bf2e6e4ba1aed6f5f473cf4
asytov00 <42006038+asytov00@users.noreply.github.com>
2025-03-21 16:58:44 +0100

    Add files via upload

---
+++ src/CrystalDeflector.cc
@@ -0,0 +1,196 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2024.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "CrystalDeflector.hh"
+
+#include "BDSAcceleratorComponent.hh"
+#include "BDSBeamPipe.hh"
+#include "BDSBeamPipeFactory.hh"
+#include "BDSBeamPipeInfo.hh"
+#include "BDSBeamPipeType.hh"
+#include "BDSColours.hh"
+#include "BDSExtent.hh"
+#include "BDSFieldBuilder.hh"
+#include "BDSFieldInfo.hh"
+#include "BDSFieldType.hh"
+#include "BDSIntegratorType.hh"
+#include "BDSMagnetGeometryType.hh"
+#include "BDSMagnetOuter.hh"
+#include "BDSMagnetOuterFactory.hh"
+#include "BDSMagnetOuterInfo.hh"
+#include "BDSMagnetStrength.hh"
+#include "BDSMagnetType.hh"
+#include "BDSMaterials.hh"
+#include "BDSSamplerPlane.hh"
+#include "BDSSamplerRegistry.hh"
+#include "BDSSDSampler.hh" // so we can convert BDSSamplerSD* to G4VSensitiveDetector*
+#include "BDSSDManager.hh"
+#include "BDSSimpleComponent.hh"
+#include "BDSUtilities.hh"
+
+#include "globals.hh" // geant4 globals / types
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4Material.hh"
+#include "G4PVPlacement.hh"
+#include "G4RotationMatrix.hh"
+#include "G4ThreeVector.hh"
+#include "G4VisAttributes.hh"
+
+#include "G4ChannelingFastSimModel.hh"
+
+CrystalDeflector::CrystalDeflector(G4String name,
+                                   G4double l,
+                                   G4double xsize,
+                                   G4double ysize,
+                                   G4double materialThickness,
+                                   G4double axisX,
+                                   G4double axisY,
+                                   G4double axisZ,
+                                   G4double horizontalWidth,
+                                   G4double bendingAngle,
+                                   G4String material,
+                                   G4String vacuumMaterial,
+                                   G4String crystalLattice,
+                                   G4String region,
+                                   G4String colour,
+                                   G4bool radiationModel):
+  BDSAcceleratorComponent(name,l, 0, "crystaldeflector"),
+  crystalXSize(xsize),
+  crystalYSize(ysize),
+  crystalMaterialThickness(materialThickness),
+  crystalAxisX(axisX),
+  crystalAxisY(axisY),
+  crystalAxisZ(axisZ),
+  containerHorizontalWidth(horizontalWidth),
+  crystalBendingAngle(bendingAngle),
+  crystalLattice(crystalLattice),
+  crystalRegionName(region),
+  crystalcolour(colour),
+  crystalRadiationModel(radiationModel)
+{
+
+  G4cout  << "name = "<< name << G4endl ;
+  G4cout  << "l = "<< l << G4endl ;
+  G4cout  << "xsize = "<< xsize << G4endl ;
+  G4cout  << "ysize = "<< ysize << G4endl ;
+  G4cout  << "materialThickness = "<< materialThickness << G4endl ;
+  G4cout  << "axisX = "<< axisX << G4endl ;
+  G4cout  << "axisY = "<< axisY << G4endl ;
+  G4cout  << "axisZ = "<< axisZ << G4endl ;
+  G4cout  << "horizontalWidth = "<< horizontalWidth << G4endl ;
+  G4cout  << "bendingAngle = "<< bendingAngle << G4endl ;
+  G4cout  << "material = "<< material << G4endl ;
+  G4cout  << "vacuumMaterial = "<< vacuumMaterial << G4endl ;
+  G4cout  << "crystalLattice = "<< crystalLattice << G4endl ;
+  G4cout  << "region = "<< region << G4endl ;
+  G4cout  << "colour = "<< colour << G4endl ;
+  G4cout  << "radiationModel = "<< radiationModel << G4endl ;
+
+  crystalMaterial   = BDSMaterials::Instance()->GetMaterial(material);
+  vacuum = BDSMaterials::Instance()->GetMaterial(vacuumMaterial);
+
+}
+
+CrystalDeflector::~CrystalDeflector()
+{
+
+}
+
+void CrystalDeflector::BuildContainerLogicalVolume()
+{
+  // containerSolid is a member of BDSAcceleratorComponent we should fill in
+  containerSolid = new G4Box(name + "_container_solid",
+                 containerHorizontalWidth * 0.5,
+                 containerHorizontalWidth * 0.5,
+			     chordLength * 0.5);
+
+
+  G4cout  << "chordLength = "<< chordLength << G4endl ;
+
+  // containerLogicalVolume is a member of BDSAcceleratorComponent we should fill in
+  containerLogicalVolume = new G4LogicalVolume(containerSolid,
+					       vacuum,
+					       name + "_container_lv");
+}
+
+void CrystalDeflector::Build()
+{
+  // Call the base class implementation of this function. This builds the container
+  // logical volume then sets some visualisation attributes for it.
+  BDSAcceleratorComponent::Build();
+
+  BuildCrystal();
+}
+
+void CrystalDeflector::BuildCrystal()
+{
+    //build crystal solid
+    G4Box* crystalSolid = new G4Box(name + "_crystal_solid",
+                   crystalXSize * 0.5,
+                   crystalYSize * 0.5,
+                   crystalMaterialThickness * 0.5);
+    RegisterSolid(crystalSolid); // for deletion by bdsim
+
+    // make a logical volume for the crystal
+    G4LogicalVolume* crystalLV = new G4LogicalVolume(crystalSolid,
+                            crystalMaterial,
+                            name + "_crystal_lv");
+
+    // visualisation attributes - make it nicely visible
+    G4VisAttributes* crystalVis = new G4VisAttributes(*BDSColours::Instance()->GetColour(crystalcolour));
+    crystalVis->SetVisibility(true);
+    crystalLV->SetVisAttributes(crystalVis);
+    RegisterVisAttributes(crystalVis);// for deletion by bdsim
+    RegisterLogicalVolume(crystalLV); // for deletion by bdsim
+
+    G4RotationMatrix* crystalRM = new G4RotationMatrix();
+    crystalRM->rotateX(crystalAxisX);
+    crystalRM->rotateY(crystalAxisY);
+    crystalRM->rotateZ(crystalAxisZ);
+    RegisterRotationMatrix(crystalRM); // for deletion by bdsim
+    G4double crystalZPos = 0*CLHEP::cm;
+    G4ThreeVector crystalPos = G4ThreeVector(0,0, crystalZPos);
+
+    //physical volume
+    auto crystalPV = new G4PVPlacement(crystalRM,
+                      crystalPos,
+                      crystalLV,
+                      name + "_crystal",
+                      containerLogicalVolume,
+                      false,
+                      0,
+                      checkOverlaps);
+    RegisterPhysicalVolume(crystalPV); // for deletion by bdsim
+
+    G4Region* crystalRegion = new G4Region(crystalRegionName);
+    crystalRegion->AddRootLogicalVolume(crystalLV);
+
+    //create the channeling model for this region
+    G4ChannelingFastSimModel* ChannelingModel = new G4ChannelingFastSimModel("ChannelingModel", crystalRegion);
+    //activate the channeling model
+    ChannelingModel->Input(crystalMaterial, crystalLattice);
+    //setting bending angle of the crystal planes (default is 0)
+    ChannelingModel->GetCrystalData()->SetBendingAngle(crystalBendingAngle,crystalLV);
+
+    if(crystalRadiationModel){
+        ChannelingModel->RadiationModelActivate();
+        ChannelingModel->GetRadiationModel()->SetRadiationAngleFactor(4.);
+        ChannelingModel->GetRadiationModel()->SetMinPhotonEnergy(1*CLHEP::MeV);
+    }
+}
---
+++ src/CrystalDeflectorConstructor.cc
@@ -0,0 +1,109 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2024.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "CrystalDeflector.hh"
+#include "CrystalDeflectorConstructor.hh"
+
+#include "BDSAcceleratorComponent.hh"
+#include "parser/element.h" // for GMAD::Element
+#include "BDSUtilities.hh"
+#include "BDSGlobalConstants.hh"
+
+#include "CLHEP/Units/SystemOfUnits.h"
+
+#include "G4Types.hh" // for G4String etc
+
+BDSAcceleratorComponent* CrystalDeflectorConstructor::Construct(GMAD::Element const* element,
+						       GMAD::Element const* /*prevElement*/,
+						       GMAD::Element const* /*nextElement*/,
+						       G4double /*currentArcLength*/,
+						       G4double /*brhoIn*/,
+						       G4double /*beta0In*/)
+{
+
+    G4double horizontalWidth = element->horizontalWidth;
+    if(horizontalWidth<0.001){horizontalWidth = 1;}
+
+    G4String vacuumMaterial = element->vacuumMaterial;
+    if(vacuumMaterial == "")
+    {vacuumMaterial=BDSGlobalConstants::Instance()->WorldMaterial();}
+
+    G4String material = element->material;
+    if(material == ""){material="G4_Si";}
+
+    // the map is supplied at run time so we must check it actually has the key
+    // to avoid throwing an exception.
+  G4String params = G4String(element->userParameters);
+    // use function from BDSUtilities to process user params string into
+    // map of strings and values.
+    std::map<G4String, G4String> map = BDS::GetUserParametersMap(params);
+
+    G4String region;
+    auto regionSearch = map.find("crystalRegion");
+    if (regionSearch != map.end())
+    {region = regionSearch->second;}
+    else
+    {region = "crystal";}
+
+    G4double bendingAngle=0;
+    auto radiusSearch = map.find("crystalBendingAngle");
+    if (radiusSearch != map.end())
+    {bendingAngle = std::stod(radiusSearch->second);}
+
+    G4String crystalLattice;
+    auto latticeSearch = map.find("crystalLattice");
+    if (latticeSearch != map.end())
+    {crystalLattice = latticeSearch->second;}
+    else
+    {crystalLattice = "(111)";}
+
+    G4String colour;
+    auto colourSearch = map.find("colour");
+    if (colourSearch != map.end())
+    {colour = colourSearch->second;}
+    else
+    {colour = "magenta";}
+
+    G4bool radiationModel=false;
+    auto radiationSearch = map.find("radiationModel");
+    if (radiationSearch != map.end())
+    {if(radiationSearch->second=="true"){radiationModel=true;};}
+
+  // Here we pull out the information we want from the parser element and construct
+  // one of our components and return it. BDSIM will delete this at the end.
+  // If you don't need to use all the parameters, use an /*inline*/ comment to
+  // avoid a compiler warning about an unused variable.
+
+  BDSAcceleratorComponent* crystal = new CrystalDeflector(element->name,
+                                                          element->l*CLHEP::m,
+                                                          element->xsize*CLHEP::m,
+                                                          element->ysize*CLHEP::m,
+                                                          element->materialThickness*CLHEP::m,
+                                                          element->axisX,
+                                                          element->axisY,
+                                                          element->axisZ,
+                                                          horizontalWidth*CLHEP::m,
+                                                          bendingAngle,
+                                                          material,
+                                                          vacuumMaterial,
+                                                          crystalLattice,
+                                                          region,
+                                                          colour,
+                                                          radiationModel);
+  return crystal;
+}
