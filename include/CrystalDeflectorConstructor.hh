eef57d6be192f0041f7d9fa8e3f3a70e80eddc0b
asytov00 <42006038+asytov00@users.noreply.github.com>
2025-03-21 17:04:17 +0100

    Update CrystalDeflectorConstructor.hh

--- include/CrystalDeflectorConstructor.hh
+++ include/CrystalDeflectorConstructor.hh
@@ -1,21 +1,17 @@
 /* 
-Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
-University of London 2001 - 2024.
-
-This file is part of BDSIM.
-
-BDSIM is free software: you can redistribute it and/or modify 
+This is free software: you can redistribute it and/or modify 
 it under the terms of the GNU General Public License as published 
-by the Free Software Foundation version 3 of the License.
+by the Free Software Foundation version 3 of the License
+http://www.gnu.org/licenses/
 
-BDSIM is distributed in the hope that it will be useful, but 
+It is distributed in the hope that it will be useful, but 
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+//Author: Alexei Sytov alexei.sytov@cern.ch
+
 #ifndef CRYSTALDEFLECTORCONSTRUCTOR_H
 #define CRYSTALDEFLECTORCONSTRUCTOR_H
 
